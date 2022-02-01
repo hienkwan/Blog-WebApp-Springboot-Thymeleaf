987ada112490ad8d818809b2824da3cc2eef7322
HienKwan <hienq127@gmail.com>
2022-02-01 15:32:55 +0700

    Add web configuration

---
+++ src/main/java/com/example/blog/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.blog.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"com.example.blog"})
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //enable static file in static folder
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
+    }
+}
