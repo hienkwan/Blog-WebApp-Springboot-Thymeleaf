ba09acba870d7d5032f43071ea01d142242fe29a
HienKwan <hienq127@gmail.com>
2022-02-05 11:54:51 +0700

    Add home method to return "Home Page"

--- src/main/java/com/example/blog/controller/HomeController.java
+++ src/main/java/com/example/blog/controller/HomeController.java
@@ -1,19 +1,28 @@
 package com.example.blog.controller;
 
+import com.example.blog.model.Post;
 import com.example.blog.model.User;
+import com.example.blog.service.PostService;
+import com.example.blog.util.Pager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private PostService postService;
 
-    @RequestMapping(value="/",method = RequestMethod.GET)
-    public String index(){
-        System.out.println("index page");
-        return "home";
+    @GetMapping(value="/home")
+    public String index(@RequestParam(defaultValue = "0") int page, Model model){
+        System.out.println("home page");
+        Page<Post> posts = postService.findAllOrderByDatePageable(page);
+        Pager pager = new Pager(posts);
+        model.addAttribute("pager",pager);
+        return "/home";
     }
 }
