6cb74329728e8ee2a76b37f27cc2b67977a5f36b
HienKwan <hienq127@gmail.com>
2022-02-10 16:33:21 +0700

    Add find user by id function

--- src/main/java/com/example/blog/service/UserService.java
+++ src/main/java/com/example/blog/service/UserService.java
@@ -1,9 +1,12 @@
 package com.example.blog.service;
 
 import com.example.blog.model.User;
+import org.bson.types.ObjectId;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
     List<User> getUsers();
+    Optional<User> getUserById(ObjectId id);
 }
--- src/main/java/com/example/blog/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/blog/service/impl/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.blog.service.impl;
 import com.example.blog.model.User;
 import com.example.blog.repository.UserRepository;
 import com.example.blog.service.UserService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,5 +19,10 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserById(ObjectId id) {
+        return userRepository.findById(id);
     }
 }
