d0d72d3842d7a4b6962355a148f031eb24c9d6f8
HienKwan <hienq127@gmail.com>
2022-02-11 20:30:05 +0700

    Add security config class and modified post controller

--- src/main/java/com/example/blog/controller/PostController.java
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -2,15 +2,12 @@ package com.example.blog.controller;
 
 import com.example.blog.model.Post;
 import com.example.blog.service.PostService;
-import com.example.blog.util.Pager;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PostController {
@@ -22,6 +19,7 @@ public class PostController {
         ObjectId idPass = new ObjectId(id);
         Post post = postService.findPostById(idPass).get();
         model.addAttribute("post",post);
+        System.out.println("Post page");
         return "/post";
     }
 }
---
+++ src/main/java/com/example/blog/security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.blog.security;
+
+public class SecurityConfig {
+}
