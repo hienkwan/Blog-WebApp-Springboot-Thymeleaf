61f42da39c52288bcc3f65b521b2c9ceb82fb651
HienKwan <hienq127@gmail.com>
2022-02-15 10:47:53 +0700

    Add jquery binding data to Edit modal form

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
+    $('.edit').on('click',function(e){
+        let id;
+        e.preventDefault();
+        let currow=$(this).closest('tr');
 
+        $('#titleEdit').val(currow.find('td:eq(0)').text());
+
+        $('#contentEdit').val(currow.find('td:eq(1)').text());
+        $('#dateEdit').val(currow.find('td:eq(2)').text());
+        $('#authorEdit').val(currow.find('td:eq(3)').text());
+        id= currow.find('td:eq(0)').attr('postid');
+        $('.title').attr('data-id',id);
+
+        //th:href="@{/posts/edit/{id}(id=${post._id})}"
+        console.log(id);
+    });
 });
